076505f773f0140ab558a7f1c43bf2f650782106
kusandipriyono <andipriyono94@gmail.com>
2024-02-04 10:02:00 +0700

    port

--- comments.js
+++ comments.js
@@ -1,1 +1,9 @@
 // Create Web Server
+// Create a web server that's going to send a response of "Hello World" for every request it receives. The server should listen on port 3000.
+
+
+const http = require('http');
+const server = http.createServer((req, res) => {
+  res.write('Hello World');
+  res.end();
+});
