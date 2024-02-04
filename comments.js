38049e236eee28499815b79b3e4dff0fe701bab9
kusandipriyono <andipriyono94@gmail.com>
2024-02-04 09:56:43 +0700

    copilot third commit

--- comments.js
+++ comments.js
@@ -1,71 +1,49 @@
-// Create Web Server for comments
-// Create and delete comments
-// Update comments
+// Create Web Server
+// ##############################
 
+// Importing Module
 const express = require('express');
-const router = express.Router();
-const Comment = require('../models/comment');
-const User = require('../models/user');
-const auth = require('../middleware/auth');
+const app = express();
+const path = require('path');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
+const { json } = require('body-parser');
 
-// Create a comment
-router.post('/create', auth, async (req, res) => {
-    try {
-        // Check if the user exists
-        const user = await User.findById(req.user.id);
-        if (!user) {
-            return res.status(404).send({ error: 'User not found' });
-        }
+// Handle Request
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-        // Create a comment
-        const comment = new Comment({
-            ...req.body,
-            owner: req.user.id
+// Set View Engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Set Static Folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Get Data
+app.get('/comments', (req, res) => {
+    fs.readFile('comments.json', 'utf-8', (err, data) => {
+        if (err) throw err;
+        let comments = JSON.parse(data);
+        res.render('comments', { comments: comments });
+    });
+});
+
+// Post Data
+app.post('/comments', (req, res) => {
+    fs.readFile('comments.json', 'utf-8', (err, data) => {
+        if (err) throw err;
+        let comments = JSON.parse(data);
+        comments.push({ id: uuidv4(), name: req.body.name, comment: req.body.comment });
+        fs.writeFile('comments.json', JSON.stringify(comments), (err) => {
+            if (err) throw err;
+            res.redirect('/comments');
         });
-        await comment.save();
-        res.status(201).send(comment);
-    } catch (error) {
-        res.status(400).send({ error: error.message });
-    }
+    });
 });
 
-// Get comments for a post
-router.get('/post/:id', auth, async (req, res) => {
-    try {
-        const comments = await Comment.find({ post: req.params.id });
-        res.status(200).send(comments);
-    } catch (error) {
-        res.status(500).send({ error: error.message });
-    }
+// Listen Port
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
 });
-
-// Update a comment
-router.patch('/update/:id', auth, async (req, res) => {
-    try {
-        const comment = await Comment.findOne({ _id: req.params.id, owner: req.user.id });
-        if (!comment) {
-            return res.status(404).send({ error: 'Comment not found' });
-        }
-        comment.text = req.body.text;
-        await comment.save();
-        res.status(200).send(comment);
-    } catch (error) {
-        res.status(500).send({ error: error.message });
-    }
-});
-
-// Delete a comment
-router.delete('/delete/:id', auth, async (req, res) => {
-    try {
-        const comment = await Comment.findOne({ _id: req.params.id, owner: req.user.id });
-        if (!comment) {
-            return res.status(404).send({ error: 'Comment not found' });
-        }
-        await comment.remove();
-        res.status(200).send(comment);
-    } catch (error) {
-        res.status(500).send({ error: error.message });
-    }
-});
-
-module.exports = router;
