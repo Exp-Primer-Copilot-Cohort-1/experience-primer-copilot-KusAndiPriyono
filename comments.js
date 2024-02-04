9f2a76008e858def2c1c0f01f806f656e0d0d54e
kusandipriyono <andipriyono94@gmail.com>
2024-02-04 09:48:49 +0700

    copilot third commit

--- comments.js
+++ comments.js
@@ -1,66 +1,89 @@
 // Create Web Server
-// 1. Load the http module
-var http = require('http');
-var fs = require('fs');
-var url = require('url');
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
 var comments = [];
-// 2. Create a server
-http.createServer(function(request, response) {
-  // 3. Parse the request containing file name
-  var pathname = url.parse(request.url).pathname;
-  // 4. Read the requested file content from file system
-  console.log("Request for " + pathname + " received.");
-  if (pathname === "/") {
-    fs.readFile("index.html", function(err, data) {
-      if (err) {
-        console.log(err);
-        response.writeHead(404, {
-          'Content-Type': 'text/html'
-        });
-      } else {
-        response.writeHead(200, {
-          'Content-Type': 'text/html'
-        });
-        response.write(data.toString());
-      }
-      response.end();
-    });
-  } else if (pathname === "/comments") {
-    if (request.method === "POST") {
-      var postData = "";
-      request.setEncoding("utf8");
-      request.addListener("data", function(postDataChunk) {
-        postData += postDataChunk;
-      });
-      request.addListener("end", function() {
-        var comment = JSON.parse(postData);
-        comments.push(comment);
-        response.writeHead(200, {
-          'Content-Type': 'application/json'
-        });
-        response.end(JSON.stringify(comments));
-      });
-    } else if (request.method === "GET") {
-      var commentString = JSON.stringify(comments);
-      response.writeHead(200, {
-        'Content-Type': 'application/json'
-      });
-      response.end(commentString);
+var commentsCount = 0;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Get all comments
+app.get('/comments', function(req, res){
+    res.json(comments);
+});
+
+// Get comment by id
+app.get('/comments/:id', function(req, res){
+    var id = req.params.id;
+    var comment = comments[id];
+    if (comment){
+        res.json(comment);
+    } else {
+        res.status(404).end();
     }
-  } else {
-    fs.readFile(pathname.substr(1), function(err, data) {
-      if (err) {
-        console.log(err);
-        response.writeHead(404, {
-          'Content-Type': 'text/html'
-        });
-      } else {
-        response.writeHead(200, {
-          'Content-Type': 'text/html'
-        });
-        response.write(data.toString());
-      }
-      response.end();
-    });
-  }
-}).listen(8080);
+});
+
+// Create comment
+app.post('/comments', function(req, res){
+    var comment = req.body.comment;
+    comments.push(comment);
+    res.status(201).end();
+});
+
+// Update comment
+app.put('/comments/:id', function(req, res){
+    var id = req.params.id;
+    var comment = req.body.comment;
+    comments[id] = comment;
+    res.status(204).end();
+});
+
+// Delete comment
+app.delete('/comments/:id', function(req, res){
+    var id = req.params.id;
+    comments.splice(id, 1);
+    res.status(204).end();
+});
+
+app.listen(3000, function(){
+    console.log('Server is running on port 3000');
+});
+```
+### 3. Create a client to test the server
+```
+javascript
+// Path: comments_client.js
+// Create a client to test the server
+var request = require('request');
+
+// Get all comments
+request('http://localhost:3000/comments', function (error, response, body) {
+    console.log(body);
+});
+
+// Get comment by id
+request('http://localhost:3000/comments/0', function (error, response, body) {
+    console.log(body);
+});
+
+// Create comment
+request.post({
+    url: 'http://localhost:3000/comments',
+    json: { comment: 'This is a new comment' }
+}, function(error, response, body){
+    console.log(response.statusCode);
+});
+
+// Update comment
+request.put({
+    url: 'http://localhost:3000/comments/0',
+    json: { comment: 'This is an updated comment' }
+}, function(error, response, body){
+    console.log(response.statusCode);
+});
+
+// Delete comment
+request.delete('http://localhost:3000/comments/0', function (error, response, body) {
+    console.log(response.statusCode);
+});
